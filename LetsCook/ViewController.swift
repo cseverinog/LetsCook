9b55f71f8d5d6c2ca2ff51b1864cda0bf568acd4
Catalina Severino <catalina.severino@globant.com>
2020-09-26 15:00:41 -0500

    Initial Commit

---
+++ LetsCook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsCook
+//
+//  Created by Catalina Severino Giraldo on 26/09/20.
+//  Copyright © 2020 catalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
