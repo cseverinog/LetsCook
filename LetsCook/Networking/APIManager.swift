51ad055577cf3b240275a1895da14b8ee1ba5b7a
Catalina Severino <catalina.severino@globant.com>
2020-09-26 15:34:59 -0500

    Get recipes and recipeDetail

---
+++ LetsCook/Networking/APIManager.swift
@@ -0,0 +1,63 @@
+//
+//  APIManager.swift
+//  LetsCook
+//
+//  Created by Catalina Severino Giraldo on 26/09/20.
+//  Copyright © 2020 catalina. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    static let recipesEndpoint = "https://gl-endpoint.herokuapp.com/recipes/"
+    static var shared = APIManager()
+    
+    private init() {}
+    
+    func getRecipes(completion: @escaping ((_ recipes: [Recipe]) -> Void)) {
+        guard let url = URL(string: APIManager.recipesEndpoint) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if error != nil {
+                print(error?.localizedDescription)
+                completion([])
+            } else {
+                guard let data = data else {
+                    completion([])
+                    return
+                }
+                
+                do {
+                    let recipes = try JSONDecoder().decode([Recipe].self, from: data)
+                    completion(recipes)
+                    
+                } catch {
+                    completion([])
+                }
+            }
+            
+        }.resume()
+    }
+    
+    func getRecipeDetail(with id: Int, completion: @escaping ((_ recipeDetail: RecipeDetail?) -> Void)) {
+        guard let url = URL(string: "\(APIManager.recipesEndpoint)\(id)") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if error != nil {
+                print(error?.localizedDescription)
+                completion(nil)
+            } else {
+                guard let data = data else {
+                    completion(nil)
+                    return
+                }
+                do {
+                    let recipeDetail = try JSONDecoder().decode(RecipeDetail.self, from: data)
+                    completion(recipeDetail)
+                } catch {
+                    completion(nil)
+                }
+            }
+        }.resume()
+    }
+}
